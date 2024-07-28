7deaaafb3298d158713faf027b0f2ffe28319232
Shourya <shourya0864@gmail.com>
2024-07-28 20:21:20 +0530

    nextapp2 with appwrite

---
+++ src/appwrite/config.ts
@@ -0,0 +1,79 @@
+import conf from "@/conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+type CreateUserAcount = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+type LoginUserAccount = {
+  email: string;
+  password: string;
+};
+
+const appwriteClient = new Client();
+
+appwriteClient
+  .setEndpoint(conf.appwwriteurl)
+  .setProject(conf.appwriteprojectid);
+
+export const account = new Account(appwriteClient);
+
+export class AppwriteService {
+  async ceateUserAcount({ email, password, name }: CreateUserAcount) {
+    try {
+      const userAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }: LoginUserAccount) { 
+                try {
+                  return await account.createEmailPasswordSession(email , password)
+                } catch (error:any) {
+                    throw error 
+                }
+  }   
+  async isLoggedin():Promise<boolean>{   
+        try { 
+          const data = await this.getCurrentUser() ; 
+          return Boolean(data) 
+            
+        } catch (error:any) {
+                    throw error 
+        }
+
+
+    return false  ;
+}
+async getCurrentUser(){ 
+    try {
+                  return   account.get()
+    } catch (error:any) {
+                console.log("getcurrentuser error", error)
+    }   
+    return null ;
+} 
+async logout(){ 
+    try{
+            return await account.deleteSession("current")
+    }catch(error:any){
+        console.log("logout error" , error)
+    }
+}
+}
+ 
+ const appwriteService =   new AppwriteService() ; 
+ export default appwriteService ; 
---
+++ src/conf/conf.ts
@@ -0,0 +1,6 @@
+const conf = {
+    appwwriteurl:String(process.env.NEXT_PUBLIC_APPWRITE_URL) , 
+    appwriteprojectid:String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)  
+}   
+
+export default conf 
